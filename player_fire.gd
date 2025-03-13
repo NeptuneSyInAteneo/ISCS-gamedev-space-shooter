0a2d8ba3518de97172a7bd316ecaa8d9f9111cad
Neptuneee11 <star.sy@student.ateneo.edu>
2025-03-13 16:25:35 +0800

    explosion works

--- player_fire.gd
+++ player_fire.gd
@@ -1,10 +1,20 @@
 extends Area2D
 
 @export var speed = 600
+@onready var animatedSprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	global_position.x += speed * delta 
-
+	
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free() # Replace with function body.
+
+func _on_body_entered(body: Node2D) -> void:
+	animatedSprite.offset.x = -30
+	animatedSprite.set_animation("explode")
+	speed = 0
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
+	pass # Replace with function body.
